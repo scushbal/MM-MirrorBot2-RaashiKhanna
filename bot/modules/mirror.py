d0595945111b5599f48c24e9d0d9404a08bfb10e
DREAM CATCHER <63229691+DreamCatcher96@users.noreply.github.com>
2022-01-10 01:01:05 +0300

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -207,16 +207,16 @@ class MirrorListener:
                 else:
                     update_all_messages()
             count = len(files)
-            msg = f'<b>Name: </b><code>{link}</code>\n\n'
-            msg += f'<b>Size: </b>{bot_utils.get_readable_file_size(size)}\n'
-            msg += f'<b>Total Files: </b>{count}'
+            msg = f'<b>📂 Movie Name : </b><code>{link}</code>\n\n'
+            msg += f'<b>💽 Size : </b>{bot_utils.get_readable_file_size(size)}\n'
+            msg += f'<b>🗂️ Total Files : </b>{count}'
             if typ != 0:
-                msg += f'\n<b>Corrupted Files: </b>{typ}'
+                msg += f'\n<b>📚 Corrupted Files : </b>{typ}'
             if self.message.chat.type == 'private':
                 sendMessage(msg, self.bot, self.update)
             else:
                 chat_id = str(self.message.chat.id)[4:]
-                msg += f'\n<b>cc: </b>{self.tag}\n\n'
+                msg += f'\n<b>📬 Count By : {self.tag}\n\n'
                 fmsg = ''
                 for index, item in enumerate(list(files), start=1):
                     msg_id = files[item]
@@ -232,14 +232,14 @@ class MirrorListener:
             return
 
         with download_dict_lock:
-            msg = f'<b>Name: </b><code>{download_dict[self.uid].name()}</code>\n\n<b>Size: </b>{size}'
-            msg += f'\n\n<b>Type: </b>{typ}'
+            msg = f'<b>📂 Movie Name : </b><code>{download_dict[self.uid].name()}</code>\n\n<b>💽 Size : </b>{size}'
+            msg += f'\n<b>📦 Type : </b>{typ}'
             if os.path.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{download_dict[self.uid].name()}'):
-                msg += f'\n<b>SubFolders: </b>{folders}'
-                msg += f'\n<b>Files: </b>{files}'
+                msg += f'\n<b>📁 SubFolders : </b>{folders}'
+                msg += f'\n<b>🗂️ Files : </b>{files}'
             buttons = button_build.ButtonMaker()
             link = short_url(link)
-            buttons.buildbutton("☁️ Drive Link", link)
+            buttons.buildbutton("💾 Drive Link 💾", link)
             LOGGER.info(f'Done Uploading {download_dict[self.uid].name()}')
             if INDEX_URL is not None:
                 url_path = requests.utils.quote(f'{download_dict[self.uid].name()}')
@@ -247,10 +247,10 @@ class MirrorListener:
                 if os.path.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{download_dict[self.uid].name()}'):
                     share_url += '/'
                     share_url = short_url(share_url)
-                    buttons.buildbutton("⚡ Index Link", share_url)
+                    buttons.buildbutton("🚀 Index Link 🚀", share_url)
                 else:
                     share_url = short_url(share_url)
-                    buttons.buildbutton("⚡ Index Link", share_url)
+                    buttons.buildbutton("🚀 Index Link 🚀", share_url)
                     if VIEW_LINK:
                         share_urls = f'{INDEX_URL}/{url_path}?a=view'
                         share_urls = short_url(share_urls)
@@ -261,7 +261,7 @@ class MirrorListener:
                 buttons.buildbutton(f"{BUTTON_FIVE_NAME}", f"{BUTTON_FIVE_URL}")
             if BUTTON_SIX_NAME is not None and BUTTON_SIX_URL is not None:
                 buttons.buildbutton(f"{BUTTON_SIX_NAME}", f"{BUTTON_SIX_URL}")
-        msg += f'\n\n<b>cc: </b>{self.tag}'
+        msg += f'\n\n<b>📬 Mirrored By : {self.tag}\n\n💫 𝑷𝒐𝒘𝒆𝒓𝒆𝒅 𝑩𝒚 : 𝑾𝒉𝒊𝒕𝑬_𝑫𝒆𝒗𝒊𝑳𝟎𝟗</b>'
         if self.isQbit and QB_SEED:
            return sendMarkup(msg, self.bot, self.update, InlineKeyboardMarkup(buttons.build_menu(2)))
         else:
