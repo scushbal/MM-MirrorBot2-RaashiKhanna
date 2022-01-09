12281a20ea5cf2af539ad4a3b8f9417119c6ea53
DREAM CATCHER <63229691+DreamCatcher96@users.noreply.github.com>
2022-01-09 23:57:14 +0300

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -62,17 +62,17 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+MwgSi5vmQEA2N2Vk")
+    buttons.buildbutton("🤖 RIPO", "https://t.me/joinchat/AAAAAE-_9UxMnUfIe4l0sQ")
+    buttons.buildbutton("OWNER 👤", "https://t.me/white_Devil09")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-This bot can mirror all your links to Google Drive!
-Type /{BotCommands.HelpCommand} to get a list of available commands
+<b>🤖 This bot can mirror all your links to Google Drive!</b>
+<b>Type /{BotCommands.HelpCommand} to get a list of available commands 🤒</b>
 '''
         sendMarkup(start_string, context.bot, update, reply_markup)
     else:
-        sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update, reply_markup)
+        sendMarkup('😎 Oops! Not Authorized user, deploy your own mirror-leech bot 🤗</b>', context.bot, update, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update)
