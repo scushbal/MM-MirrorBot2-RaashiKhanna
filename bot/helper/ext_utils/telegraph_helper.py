f0343e791b596cde359ab499c10518eb748f7766
DREAM CATCHER <63229691+DreamCatcher96@users.noreply.github.com>
2022-01-10 01:33:23 +0300

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -57,4 +57,4 @@ class TelegraphHelper:
 			return self.edit_page(path, title, content)
 
 
-telegraph=TelegraphHelper('Mirror-Leech-Telegram-Bot', 'https://github.com/anasty17/mirror-leech-telegram-bot')
+telegraph=TelegraphHelper('@WhitE_DeviL09', 'https://t.me/white_devil09')
