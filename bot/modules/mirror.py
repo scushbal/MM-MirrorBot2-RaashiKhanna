431e57bdf0a80dc48ad9242c884a980137babfc0
DREAM CATCHER <63229691+DreamCatcher96@users.noreply.github.com>
2022-01-10 11:41:43 +0300

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -233,7 +233,7 @@ class MirrorListener:
 
         with download_dict_lock:
             msg = f'<b>📂 Movie Name : </b><code>{download_dict[self.uid].name()}</code>\n\n<b>💽 Size : {size}</b>'
-            msg += f'\n\n<b>📦 Type : {typ}</b>'
+            msg += f'\n<b>📦 Type : {typ}</b>'
             if os.path.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{download_dict[self.uid].name()}'):
                 msg += f'\n<b>📂 SubFolders : {folders}</b>'
                 msg += f'\n<b>🗂️ Files: {files}</b>'
@@ -261,7 +261,7 @@ class MirrorListener:
                 buttons.buildbutton(f"{BUTTON_FIVE_NAME}", f"{BUTTON_FIVE_URL}")
             if BUTTON_SIX_NAME is not None and BUTTON_SIX_URL is not None:
                 buttons.buildbutton(f"{BUTTON_SIX_NAME}", f"{BUTTON_SIX_URL}")
-        msg += f'\n\n<b>📬 Mirrored By : {self.tag}</b>/n/n<b>🔰 Powered By : #WhitE_DeviL09'
+        msg += f'\n\n<b>📬 Mirrored By : {self.tag}</b>/n/n<b>🔰 Powered By : WhitE_DeviL09</b>'
         if self.isQbit and QB_SEED:
            return sendMarkup(msg, self.bot, self.update, InlineKeyboardMarkup(buttons.build_menu(2)))
         else:
